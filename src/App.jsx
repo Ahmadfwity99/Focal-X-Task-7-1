21861a9e1cc46ac0be57fe389e036a27ee2aaa9a
Ahmad Fwity <ahmad.fwaity@gmail.com>
2024-11-30 12:01:22 +0300

    demo

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path='/Focal-X-Task-7-1' element={<Home />} />
         <Route path='/about' element={<About />} />
         <Route path='/contact' element={<Contact />} />
       </Routes>
